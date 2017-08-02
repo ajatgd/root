5417236f184a7d8272a8e587e4e2a8e475f2edc6
ajatgd <akshayvashistha1995@gmail.com>
2017-08-02 13:40:26 +0530

    Changing MethodDAE to parse DAE Layer

--- tmva/tmva/inc/TMVA/MethodDAE.h
+++ tmva/tmva/inc/TMVA/MethodDAE.h
@@ -36,6 +36,7 @@
 #include "TMVA/DNN/DAE/DenoiseAE.h"
 #include "TMVA/MethodBase.h"
 #include "TString.h"
+#include "TMVA/DNN/DAE/StackedNet.h"
 
 #include "TMVA/DNN/Architectures/Reference.h"
 
@@ -55,50 +56,47 @@ using namespace TMVA::DNN;
 
 namespace TMVA {
 
+  struct TTrainingSettings {
+    size_t batchSize;
+    size_t testInterval;
+    size_t convergenceSteps;
+    //DNN::ERegularization regularization;
+    Double_t learningRate;
+    //Double_t momentum;
+    Double_t weightDecay;
+    Double_t corruption;
+    bool multithreading;
+  };
+
 class MethodDAE : public MethodBase {
 public:
   using Architecture_t = TReference<Double_t>;
   using Matrix_t = typename Architecture_t::Matrix_t;
-  using TDAE = TDAE<Architecture_t>;
 
 private:
-  using LayoutVector_t = std::vector<std::tuple>;
+  using LayoutVector_t = std::vector<Int_t>;
   using KeyValueVector_t = std::vector<std::map<TString, TString>>;
 
-  struct TTrainingSettings {
-    size_t batchSize;
-    Double_t learningRate;
-    Double_t corruptionLevel;
-  };
+
+  TDAE<Architecture_t>* net;
   EInitialization fWeightInitialization; ///< The initialization method
-  EOutputFunction
-      fOutputFunction; ///< The output function for making the predictions
+  EOutputFunction fOutputFunction; ///< The output function for making the predictions
 
   TString fLayoutString; ///< The string defining the layout of the DAE
-  TString
-      fErrorStrategy; ///< The string defining the error strategy for training
-  TString
-      fTrainingStrategyString; ///< The string defining the training strategy
+  TString fErrorStrategy; ///< The string defining the error strategy for training
+  TString fTrainingStrategyString; ///< The string defining the training strategy
   TString fWeightInitializationString; ///< The string defining the weight
-                                       ///initialization method
-  TString
-      fArchitectureString; ///< The string defining the architecure: CPU or GPU
+  TString fArchitectureString; ///< The string defining the architecure: CPU or GPU
   bool fResume;
 
   LayoutVector_t fLayout; ///< Dimensions and activation functions of each layer
-  std::vector<TTrainingSettings>
-      fTrainingSettings; ///< The vector defining each training strategy
+  std::vector<TTrainingSettings> fTrainingSettings; ///< The vector defining each training strategy
 
   void Init();
 
   // the option handling methods
   void DeclareOptions();
   void ProcessOptions();
-
-  TDAE dae;
-
-  DNN::EInitialization fWeightInitialization;
-  DNN::EOutputFunction fOutputFunction;
 
   // Write and read weights from an XML file
   static inline void WriteMatrixXML(void *parent, const char *name,
@@ -107,6 +105,16 @@ private:
                                    TMatrixT<Double_t> &X);
 
   ClassDef(MethodDAE, 0);
+
+  /*template <typename T>
+  TString fetchValue (const std::map<TString, TString>& keyValueMap, TString key);
+  T MethodDAE::fetchValue(const std::map<TString,TString>& keyValueMap, TString key, T defaultValue);
+  std::vector<double> fetchValue(const std::map<TString, TString> & keyValueMap, TString key, std::vector<double> defaultValue);
+  int fetchValue(const std::map<TString,TString>& keyValueMap, TString key, int defaultValue);
+  double fetchValue (const std::map<TString,TString>& keyValueMap, TString key, double defaultValue);
+  TString fetchValue (const std::map<TString,TString>& keyValueMap, TString key, TString defaultValue);
+  bool fetchValue (const std::map<TString,TString>& keyValueMap, TString key, bool defaultValue); */
+
 
 protected:
   void GetHelpMessage() const;
@@ -144,6 +152,10 @@ public:
 
   // ranking of input variables
   const Ranking *CreateRanking();
+
+  KeyValueVector_t ParseKeyValueString(TString parseString,
+                                      TString blockDelim,
+                                      TString tokenDelim);
 };
 } // namespace TMVA
 
---
+++ tmva/tmva/inc/TMVA/Parsing.h
@@ -0,0 +1,44 @@
+#ifndef PARSING
+#define PARSING
+
+#include "TString.h"
+#include "TObject.h"
+#include <vector>
+#include <map>
+
+#include "TString.h"
+
+#include "TMVA/Tools.h"
+
+
+
+
+
+
+
+namespace TMVA
+{
+  namespace DNNParsing {
+
+
+
+  TString fetchValue (const std::map<TString, TString>& keyValueMap, TString key);
+
+  std::vector<double> fetchValue(const std::map<TString, TString> & keyValueMap, TString key, std::vector<double> defaultValue);
+
+  int fetchValue(const std::map<TString,TString>& keyValueMap, TString key, int defaultValue);
+
+  double fetchValue (const std::map<TString,TString>& keyValueMap, TString key, double defaultValue);
+
+  TString fetchValue (const std::map<TString,TString>& keyValueMap, TString key, TString defaultValue);
+
+  bool fetchValue (const std::map<TString,TString>& keyValueMap, TString key, bool defaultValue);
+
+
+
+  }
+
+}
+
+
+#endif
