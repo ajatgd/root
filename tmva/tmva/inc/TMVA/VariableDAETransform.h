d2052c4e8997a9734f741746d104c2b0cca7ae0d
mhuwiler <mhuwiler@pcphsft81.dyndns.cern.ch>
2017-08-16 19:48:02 +0200

    Fixed segmentation fault issue in Predict

--- tmva/tmva/inc/TMVA/VariableDAETransform.h
+++ tmva/tmva/inc/TMVA/VariableDAETransform.h
@@ -94,7 +94,8 @@ namespace TMVA {
       std::vector<DNN::TDeepAutoEncoder<Architecture_t>* > fAutoEncoder; 
       //TCompressionLayer fEncoder; 
 
-      std::vector<Matrix_t> input; 
+      std::vector<std::vector<Matrix_t> > input;   // One DAE per class plus one extra for all classes together. 
+      std::vector<Matrix_t> totalInput; 
       std::vector<Matrix_t> output; 
 
       Int_t numCompressedUnits; 
